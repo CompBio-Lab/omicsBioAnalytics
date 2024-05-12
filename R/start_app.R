c0d1da55e2176f66c1ea9d5ad908eec64309eb5c
Amrit Singh <amrit.singh@hli.ubc.ca>
2024-05-11 22:51:19 -0700

    updatae startup

--- R/start_app.R
+++ R/start_app.R
@@ -14,7 +14,7 @@ prepareData = function(omics_data, demo, response_var, reference){
   })
 
   ## biomarker analysis
-  mods <- model_training(omics_data, response, alphaMin=0, alphaMax=1,
+  mods <- omicsBioAnalytics::model_training(omics_data, response, alphaMin=0, alphaMax=1,
                          alphalength=5, kfolds=5, n_repeats = 2)
   return(list(response_var=response_var, demo=demo, reference=reference, omics_data=omics_data,
               pca=pca, response=response, de_results=de_results, mods=mods))
